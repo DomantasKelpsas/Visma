e70cab36c8bc7546b0d5506e7b28c0922b43db4c
Domantas K <domantas.kelpsas@gmail.com>
2020-12-30 17:05:40 +0200

    first commit

---
+++ VismaProject/VismaProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace VismaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+    }
+}

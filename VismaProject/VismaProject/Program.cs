aa5f17f0b77aabbd9b3550598556d68d0b2f670b
Domantas K <domantas.kelpsas@gmail.com>
2020-12-31 16:10:05 +0200

    reduce stock by new order

--- VismaProject/VismaProject/Program.cs
+++ VismaProject/VismaProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using CsvHelper.Configuration;
 using Microsoft.Extensions.Configuration;
 using VismaProject.Mappers;
@@ -59,11 +60,11 @@ namespace VismaProject
                     break;
                 case "m":
                         printData<MenuItem>(resultDataOfMenu);
-                        menuItemAcions(ref _MenuItemService, ref resultDataOfMenu, pathsToCSV[1]);
+                        menuItemAcions(ref _MenuItemService, ref resultDataOfMenu, pathsToCSV);
                         break;
                 case "o":
                         printData<OrderItem>(resultDataOfOrder);
-                        orderItemAcions(ref _OrderItemService, ref resultDataOfOrder, pathsToCSV[2]);
+                        orderItemAcions(ref _OrderItemService, ref resultDataOfOrder, pathsToCSV);
                         break;
                     case "x":
                         refresh = false;
@@ -98,11 +99,10 @@ namespace VismaProject
             resultData.Add(StockItem);         
             _ItemService.WriteCSVFile(path, resultData);
         }
-
-        static void addMenuItem(ref MenuItemService _ItemService, ref List<MenuItem> resultData, string path)
+        static void addMenuItem(ref MenuItemService _ItemService, ref List<MenuItem> resultData, string[] pathsToCSV)
         {
 
-            checkValidItemIds<MenuItem>(resultData, "");
+
 
             Console.Write("Insert item id: ");
             int id = Convert.ToInt32(Console.ReadLine().Trim());
@@ -111,15 +111,21 @@ namespace VismaProject
             Console.Write("Insert item Products: ");
             string products = Console.ReadLine().Trim();
 
+            var _StockItemService = new StockItemService();
+            var _resultData = _StockItemService.ReadCSVFile(pathsToCSV[0]);
+            var valid = checkValidItemIds<StockItem>(_resultData, products);
 
-            MenuItem menuItem = new MenuItem();
-            menuItem.Id = id;
-            menuItem.Name = name;
-            menuItem.Products = products;      
-            resultData.Add(menuItem);
-            _ItemService.WriteCSVFile(path, resultData);
+            if (valid) {
+                MenuItem menuItem = new MenuItem();
+                menuItem.Id = id;
+                menuItem.Name = name;
+                menuItem.Products = products;
+                resultData.Add(menuItem);
+                _ItemService.WriteCSVFile(pathsToCSV[1], resultData);
+            }
+            else Console.WriteLine($"Could not find such items ({products})");
         }
-        static void addOrderItem(ref OrderItemService _ItemService, ref List<OrderItem> resultData, string path)
+        static void addOrderItem(ref OrderItemService _ItemService, ref List<OrderItem> resultData, string[] pathsToCSV)
         {
 
             Console.Write("Insert order id: ");
@@ -127,14 +133,24 @@ namespace VismaProject
             Console.Write("Insert menu Items: ");
             string products = Console.ReadLine().Trim();
 
+            var _MenuItemService = new MenuItemService();
+            var _resultData = _MenuItemService.ReadCSVFile(pathsToCSV[1]);
+            var valid = checkValidItemIds<MenuItem>(_resultData, products);
+
+            if (valid)
+            {
+
+                reduceFromStock(products,pathsToCSV);
+
             OrderItem OrderItem = new OrderItem();
             OrderItem.Id = id;
             OrderItem.DateTime = DateTime.Now;
             OrderItem.MenuItems = products;
             resultData.Add(OrderItem);
-            _ItemService.WriteCSVFile(path, resultData);
+            _ItemService.WriteCSVFile(pathsToCSV[2], resultData);
+            }
+            else Console.WriteLine($"Could not find such items ({products})");
         }
-
         static void removeItem<I>(ref List<I> resultData, int removeId)
         {
             for (int i = 0; i < resultData.Count; i++)
@@ -172,7 +188,7 @@ namespace VismaProject
             }
         }
 
-        static void menuItemAcions(ref MenuItemService _itemService, ref List<MenuItem> resultData, string pathToFile)
+        static void menuItemAcions(ref MenuItemService _itemService, ref List<MenuItem> resultData, string[] pathsToCSV)
         {
 
             Console.Write("insert = 1 \t | \t delete = 2 \t | \t view = 3 \t: ");
@@ -180,13 +196,13 @@ namespace VismaProject
             {
 
                 case 1:
-                    addMenuItem(ref _itemService, ref resultData, pathToFile);
+                    addMenuItem(ref _itemService, ref resultData, pathsToCSV);
                     break;
                 case 2:
                     Console.Write("Type Id of item to remove: ");
                     int removeId = Convert.ToInt32(Console.ReadLine().Trim());
                     removeItem(ref resultData, removeId);
-                    _itemService.WriteCSVFile(pathToFile, resultData);
+                    _itemService.WriteCSVFile(pathsToCSV[1], resultData);
                     break;
                 case 3:
                     printData<MenuItem>(resultData);
@@ -195,7 +211,7 @@ namespace VismaProject
             }
         }
 
-        static void orderItemAcions(ref OrderItemService _itemService, ref List<OrderItem> resultData, string pathToFile)
+        static void orderItemAcions(ref OrderItemService _itemService, ref List<OrderItem> resultData, string[] pathsToCSV)
         {
 
             Console.Write("insert = 1 \t | \t delete = 2 \t | \t view = 3 \t: ");
@@ -203,13 +219,13 @@ namespace VismaProject
             {
 
                 case 1:
-                    addOrderItem(ref _itemService, ref resultData, pathToFile);
+                    addOrderItem(ref _itemService, ref resultData,pathsToCSV);
                     break;
                 case 2:
                     Console.Write("Type Id of item to remove: ");
                     int removeId = Convert.ToInt32(Console.ReadLine().Trim());
                     removeItem(ref resultData, removeId);
-                    _itemService.WriteCSVFile(pathToFile, resultData);
+                    _itemService.WriteCSVFile(pathsToCSV[2], resultData);
                     break;
                 case 3:
                     printData<OrderItem>(resultData);
@@ -218,35 +234,69 @@ namespace VismaProject
             }
         }
 
-        static void itemAcions<I>(ref ItemService<I> _itemService, ref List<I> resultData, string pathToFile)
-        {
+        //static void itemAcions<I>(ref List<I> resultData, string pathToFile)
+        //{
+           
+        //    Console.Write("insert = 1 \t | \t delete = 2 \t | \t view = 3 \t: ");
+        //    switch (Convert.ToInt32(Console.ReadLine().Trim()))
+        //    {
 
-            Console.Write("insert = 1 \t | \t delete = 2 \t | \t view = 3 \t: ");
-            switch (Convert.ToInt32(Console.ReadLine().Trim()))
-            {
+        //        case 1:
+        //            if (resultData.GetType() == typeof(List<StockItem>))
+        //            {
+        //                var _itemService = new StockItemService();                     
+        //                var _resultData = _itemService.ReadCSVFile(pathToFile);
+        //                addStockItem(ref _itemService, ref _resultData, pathToFile);
+        //            }
+        //            else if (resultData.GetType() == typeof(List<MenuItem>))
+        //            {
+        //                var _itemService = new MenuItemService();
+        //                var _resultData = _itemService.ReadCSVFile(pathToFile);
+        //                addMenuItem(ref _itemService, ref _resultData, pathToFile);
+        //            }
+        //            else if (resultData.GetType() == typeof(List<OrderItem>))
+        //            {
+        //                var _itemService = new OrderItemService();
+        //                var _resultData = _itemService.ReadCSVFile(pathToFile);
+        //                addOrderItem(ref _itemService, ref _resultData, pathToFile);
+        //            }
+        //            break;
+        //        case 2:
+        //            Console.Write("Type Id of item to remove: ");
+        //            int removeId = Convert.ToInt32(Console.ReadLine().Trim());
+        //            removeItem(ref resultData, removeId);
+        //            if (resultData.GetType() == typeof(List<OrderItem>))
+        //            {
+        //                var _itemService = new OrderItemService();
+        //                _itemService.WriteCSVFile(pathToFile, resultData);
+        //            }
+        //            break;
+        //        case 3:
+        //            printData<I>(resultData);
+        //            break;
 
-                case 1:
-                   
-                    //addOrderItem(ref _itemService, ref resultData, pathToFile);
-                    break;
-                case 2:
-                    Console.Write("Type Id of item to remove: ");
-                    int removeId = Convert.ToInt32(Console.ReadLine().Trim());
-                    removeItem(ref resultData, removeId);
-                    _itemService.WriteCSVFile(pathToFile, resultData);
-                    break;
-                case 3:
-                    printData<I>(resultData);
-                    break;
-
-            }
-        }
+        //    }
+        //}
         static bool checkValidItemIds<I>(List<I> resultData, string ids) {
 
-            foreach (var item in resultData)
-                Console.WriteLine(item.GetType().GetProperty("Id").GetValue(item, null));
 
+            var idList = ids.Trim().Split(' ').Select(Int32.Parse).ToList();
+            int idList_size = idList.Count;
+            int validCount = 0;
+            foreach (int i in idList)
+            {
+                foreach (var item in resultData)
+                {
+                   var id = item.GetType().GetProperty("Id").GetValue(item, null);
+                    if (i == (int)id) {
+                        validCount++;
+                        break;                   
+                    }
+                }
+            }
+            if (validCount == idList_size)
                 return true;
+            else return false;
         }
 
 
@@ -254,6 +304,36 @@ namespace VismaProject
 
             foreach (var item in resultData)
                 Console.WriteLine(item.ToString());
+        }
+
+        static bool reduceFromStock(string ids, string[] pathsToCSV) {
+
+            var menuIds = ids.Trim().Split(' ').Select(Int32.Parse).ToList();
+            
+            var _StockItemService = new StockItemService();
+            var resultDataOfStock = _StockItemService.ReadCSVFile(pathsToCSV[0]);
+
+            var _MenuItemService = new MenuItemService();
+            var resultDataOfMenu = _MenuItemService.ReadCSVFile(pathsToCSV[1]);
+
+            foreach (int mId in menuIds)
+                foreach (MenuItem mItem in resultDataOfMenu) {
+                    if (mItem.Id == mId) {
+                        string Products = mItem.Products;
+                        var stockIds = Products.Trim().Split(' ').Select(Int32.Parse).ToList();
+                        foreach (int sId in stockIds)
+                            foreach (StockItem sItem in resultDataOfStock)
+                                if (sItem.Id == sId)
+                                    if (sItem.PortionCount > 0)
+                                    {
+                                        sItem.PortionCount--;
+                                        _StockItemService.WriteCSVFile(pathsToCSV[0], resultDataOfStock);
+                                    }
+                                    else return false;
+
+                    }
+                }
+            return true;
         }
     }
 }
